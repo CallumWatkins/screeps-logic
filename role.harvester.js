e6306da24d35d9069dc355c02724f6605f8aa01b
Callum Watkins <callum@callumwatkins.com>
2019-09-21 00:58:28 +0100

    Update role.harvester to choose closest target or move to spawn

--- role.harvester.js
+++ role.harvester.js
@@ -20,18 +20,17 @@ var roleHarvester = {
         if (creep.memory.harvesting) {
             creep.harvestNearestEnergyByPath();
         } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: structure => (structure.structureType == STRUCTURE_EXTENSION ||
-                                      structure.structureType == STRUCTURE_SPAWN ||
-                                      structure.structureType == STRUCTURE_TOWER)
-                                      && structure.energy < structure.energyCapacity
-                
+            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s => (s.structureType === STRUCTURE_EXTENSION ||
+                              s.structureType === STRUCTURE_SPAWN ||
+                              s.structureType === STRUCTURE_TOWER)
+                             && s.energy < s.energyCapacity
             });
             
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            if (target) {
+                creep.moveAndTransferEnergy(target);
+            } else {
+                creep.moveTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS));
             }
         }
     }
