f89eb86cbf4c116a0145f532105a76d2f5b3d59a
Callum Watkins <callum@callumwatkins.com>
2019-09-21 01:00:12 +0100

    Update role.upgrader with creep prototype methods

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,24 +2,20 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.memory.upgrading && creep.carry.energy == 0) {
+        if (creep.memory.upgrading && creep.isCarryingZeroEnergy()) {
             creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
+            creep.say('⛏️harvest');
         }
         
-        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.upgrading && creep.isCarryingMaximumEnergy()) {
             creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
+            creep.say('⚡upgrade');
         }
 
         if (creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-            }
+            creep.moveAndUpgradeController(creep.room.controller);
         } else {
-            if (!creep.pickupNearByDroppedEnergy()) {
-                creep.harvestNearestEnergy();
-            }
+            creep.withdrawNearByTombstoneEnergy() || creep.pickupNearByDroppedEnergy() || creep.harvestNearestEnergyByRange(creep.room.controller.pos);
         }
     }
 };
