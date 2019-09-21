502d06fbfcf77b28a0243c411b9445847273d325
Callum Watkins <callum@callumwatkins.com>
2019-09-21 01:07:49 +0100

    Rename prototype.creep to creep.prototype
    Disable reusePath.
    Improve formatting.
    Add roles object and run method.

--- creep.prototype.js
+++ creep.prototype.js
@@ -1,4 +1,15 @@
 module.exports = function () {
+    Creep.prototype.ROLES = {
+        'harvester': require('creep.role.harvester'),
+        'upgrader': require('creep.role.upgrader'),
+        'builder': require('creep.role.builder'),
+        'repairer': require('creep.role.repairer')
+    };
+    
+    Creep.prototype.run = function () {
+        this.ROLES[this.memory.role].run(this);
+    };
+    
     /**
      * Pick up any droppd energy adjacent to the current location.
      * 
@@ -6,10 +17,10 @@ module.exports = function () {
      */
     Creep.prototype.pickupNearByDroppedEnergy = function () {
         return this.pickupNearByDroppedResource(RESOURCE_ENERGY);
-    }
+    };
     
     Creep.prototype.pickupNearByDroppedResource = function (resourceType) {
-        var closestDroppedResource = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: r => r.resourceType === resourceType});
+        var closestDroppedResource = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: r => r.resourceType === resourceType });
         if (this.pos.isNearTo(closestDroppedResource)) {
             this.pickup(closestDroppedResource);
             this.say('💲 pickup');
@@ -17,11 +28,11 @@ module.exports = function () {
         }
         
         return false;
-    }
+    };
     
     Creep.prototype.withdrawNearByTombstoneEnergy = function () {
         return this.withdrawNearByTombstoneResource(RESOURCE_ENERGY);
-    }
+    };
     
     /**
      * 
@@ -36,7 +47,7 @@ module.exports = function () {
         }
         
         return false;
-    }
+    };
     
     /**
      * Move to and harvest the closest actve energy source by path.
@@ -47,9 +58,9 @@ module.exports = function () {
         fromPosition = fromPosition || this.pos;
         var closestActiveSource = fromPosition.findClosestByPath(FIND_SOURCES_ACTIVE);
         if (this.harvest(closestActiveSource) === ERR_NOT_IN_RANGE) {
-            this.moveTo(closestActiveSource, {visualizePathStyle: {stroke: '#ffbf00', opacity: 0.2}});
+            this.moveTo(closestActiveSource, {reusePath: 0, visualizePathStyle: {stroke: '#ffbf00', opacity: 0.2}});
         }
-    }
+    };
     
     /**
      * Move to and harvest the closest actve energy source by range.
@@ -60,43 +71,43 @@ module.exports = function () {
         fromPosition = fromPosition || this.pos;
         var closestActiveSource = fromPosition.findClosestByRange(FIND_SOURCES_ACTIVE);
         if (this.harvest(closestActiveSource) === ERR_NOT_IN_RANGE) {
-            this.moveTo(closestActiveSource, {visualizePathStyle: {stroke: '#ffbf00', opacity: 0.2}});
+            this.moveTo(closestActiveSource, {reusePath: 0, visualizePathStyle: {stroke: '#ffbf00', opacity: 0.2}});
         }
-    }
+    };
     
     Creep.prototype.moveAndTransferEnergy = function (target) {
         this.moveAndTransfer(target, RESOURCE_ENERGY);
-    }
+    };
     
     Creep.prototype.moveAndTransfer = function (target, resourceType) {
         if (this.transfer(target, resourceType) === ERR_NOT_IN_RANGE) {
-            this.moveTo(target, {visualizePathStyle: {stroke: '#ffffff', opacity: 0.2}});
+            this.moveTo(target, {reusePath: 0, visualizePathStyle: {stroke: '#ffffff', opacity: 0.2}});
         }
-    }
+    };
     
     Creep.prototype.moveAndBuild = function (constructionSite) {
         if (this.build(constructionSite) === ERR_NOT_IN_RANGE) {
-            this.moveTo(constructionSite, {visualizePathStyle: {stroke: '#1e7dff', opacity: 0.2}});
+            this.moveTo(constructionSite, {reusePath: 0, visualizePathStyle: {stroke: '#1e7dff', opacity: 0.2}});
         }
-    }
+    };
     
     Creep.prototype.moveAndUpgradeController = function (controller) {
         if (this.upgradeController(controller) === ERR_NOT_IN_RANGE) {
-            this.moveTo(controller, {visualizePathStyle: {stroke: '#ffffff', opacity: 0.2}});
+            this.moveTo(controller, {reusePath: 0, visualizePathStyle: {stroke: '#ffffff', opacity: 0.2}});
         }
-    }
+    };
     
     Creep.prototype.moveAndRepair = function (target) {
         if (this.repair(target) === ERR_NOT_IN_RANGE) {
-            this.moveTo(target, {visualizePathStyle: {stroke: '#008000', opacity: 0.2}});
+            this.moveTo(target, {reusePath: 0, visualizePathStyle: {stroke: '#008000', opacity: 0.2}});
         }
-    }
+    };
     
     Creep.prototype.isCarryingMaximumEnergy = function () {
         return this.carry[RESOURCE_ENERGY] === this.carryCapacity;
-    }
+    };
     
     Creep.prototype.isCarryingZeroEnergy = function () {
         return this.carry[RESOURCE_ENERGY] === 0;
-    }
+    };
 };
